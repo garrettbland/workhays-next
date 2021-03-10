447a3a0a33f00fa702eaea8611e5d9fa9e0037be
garrettbland <gmorganbland@gmail.com>
2021-03-09 22:43:50 -0600

    working on footer styling

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,44 +5,106 @@ type FooterLinkType = {
     link: string
 }
 
-const FOOTER_LINKS: FooterLinkType[] = [
+const EMPLOYER_LINKS: FooterLinkType[] = [
     {
-        title: 'About',
-        link: '/about',
+        title: 'Post a job',
+        link: '/signup',
     },
     {
+        title: 'Sign Up',
+        link: '/sign-up',
+    },
+    {
+        title: 'Sign In',
+        link: '/sign-in',
+    },
+    {
+        title: 'Help',
+        link: '/help',
+    },
+]
+
+const LINKS: FooterLinkType[] = [
+    {
         title: 'Contact',
-        link: '/contact',
+        link: '/signup',
+    },
+    {
+        title: 'About',
+        link: '/sign-up',
     },
     {
         title: 'Privacy',
-        link: '/privacy',
+        link: '/sign-in',
+    },
+    {
+        title: 'Terms',
+        link: '/help',
     },
 ]
 
 const Footer = () => {
     return (
-        <div className="border-t border-blue-50 overflow-hidden">
-            <div className="max-w-4xl mx-auto p-4">
-                <div className="grid grid-cols-1 md:grid-cols-3">
-                    <div>Work Hays</div>
+        <div className="border-t border-gray-200 overflow-hidden">
+            <div className="max-w-4xl mx-auto px-4 py-10">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-12 md:gap-4">
+                    <div className="text-lg font-bold">Work Hays</div>
                     <div>
-                        <div className="uppercase">For Employers</div>
-                        <div>Post a job</div>
-                        <div>Sign Up</div>
-                        <div>Sign In</div>
-                        <div>Help</div>
+                        <div className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
+                            For Employers
+                        </div>
+                        <ul className="mt-4 space-y-2">
+                            {EMPLOYER_LINKS.map(({ title, link }, index) => (
+                                <li key={index}>
+                                    <a
+                                        href={link}
+                                        className="text-base text-gray-600 hover:text-gray-900 hover:underline"
+                                    >
+                                        {title}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                     <div>
-                        <div className="uppercase">Links</div>
-                        <div>Contact</div>
-                        <div>About</div>
-                        <div>Privacy</div>
-                        <div>Terms</div>
+                        <div className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
+                            Links
+                        </div>
+                        <ul className="mt-4 space-y-2">
+                            {LINKS.map(({ title, link }, index) => (
+                                <li key={index}>
+                                    <a
+                                        href={link}
+                                        className="text-base text-gray-600 hover:text-gray-900 hover:underline"
+                                    >
+                                        {title}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
-                <div className="text-center py-5">
-                    Copyright {new Date().getFullYear()} Work Hays
+                <div className="py-5 mt-10 border-t border-gray-200 flex flex-row justify-between items-center">
+                    <div className="text-base text-gray-400">
+                        © {new Date().getFullYear()} Work Hays. All rights reserved.
+                    </div>
+                    <Link href="#">
+                        <a className="text-gray-400 hover:text-gray-500">
+                            <span className="sr-only">Facebook</span>
+                            <svg
+                                className="h-6 w-6"
+                                fill="currentColor"
+                                viewBox="0 0 24 24"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                        </a>
+                    </Link>
                 </div>
             </div>
         </div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -17,10 +17,10 @@ const DESKTOP_LINKS = [
 
 const Navbar = () => {
     return (
-        <div className="border-b border-blue-50 overflow-hidden">
+        <div className="border-b border-gray-200 overflow-hidden">
             <div className="max-w-4xl mx-auto px-4">
                 <div className="flex flex-row items-center justify-between">
-                    <div>Work Hays</div>
+                    <div className="text-lg font-bold">Work Hays</div>
                     <div className="grid grid-flow-col grid-col-4 gap-2">
                         {DESKTOP_LINKS.map(({ title, link }, index) => (
                             <DesktopLink key={index} title={title} link={link} />
