84e80bba7235cc3e1a21cc9783799fe43a68fcb2
garrettbland <gmorganbland@gmail.com>
2021-03-09 22:55:37 -0600

    more styling updates to footer and navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,5 @@
 import { DesktopLink } from '../components/navbarLink'
+import Link from 'next/link'
 
 const DESKTOP_LINKS = [
     {
@@ -20,7 +21,13 @@ const Navbar = () => {
         <div className="border-b border-gray-200 overflow-hidden">
             <div className="max-w-4xl mx-auto px-4">
                 <div className="flex flex-row items-center justify-between">
-                    <div className="text-lg font-bold">Work Hays</div>
+                    <div>
+                        <Link href="/">
+                            <a className="text-lg font-bold text-gray-700 hover:text-gray-900 hover:underline inline-block">
+                                Work Hays
+                            </a>
+                        </Link>
+                    </div>
                     <div className="grid grid-flow-col grid-col-4 gap-2">
                         {DESKTOP_LINKS.map(({ title, link }, index) => (
                             <DesktopLink key={index} title={title} link={link} />
