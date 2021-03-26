b63949c84b77f9998c4eb44322c75523383f8c92
garrettbland <gmorganbland@gmail.com>
2021-03-25 23:33:56 -0500

    setting up new types

--- utils/types.ts
+++ utils/types.ts
@@ -3,3 +3,51 @@ export type LinkType = {
     href: string
     target?: string
 }
+
+/**
+ * Setting industry key if users wanted to sign up for
+ * alerts for when specific industries added new job
+ * listings.
+ */
+export type Subscriber = {
+    id: string
+    email: string
+    industry: string | null
+}
+
+export type User = {
+    id: string
+    first_name: string
+    last_name: string
+    email: string
+    disabled: boolean
+    role: 'user' | 'administrator'
+}
+
+export type Job = {
+    id: string
+    employer_id: string
+    title: string
+    type: 'full_time' | 'part_time'
+    description: string
+    industry: string
+    application_link?: string
+    status: 'active' | 'inactive' | 'archived'
+}
+
+export type Employer = {
+    id: string
+    owner_user_id: string
+    users: {
+        user_id: string
+    }[]
+    title: string
+    description: string
+    contact: string
+    email?: string
+    phone?: string
+    logo_url?: string
+    website_url?: string
+    facebook_url?: string
+    twitter_url?: string
+}
