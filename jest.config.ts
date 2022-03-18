c5ff7bf3b558f8c19aad23c26429c4c30c3dd1cd
Garrett Bland <gmorganbland@gmail.com>
2022-03-17 23:05:06 -0500

    feat: major version bumps, jest dom, tailwind v3

--- jest.config.ts
+++ jest.config.ts
@@ -2,27 +2,10 @@ import type { Config } from '@jest/types'
 // Sync object
 const config: Config.InitialOptions = {
     verbose: true,
+    setupFilesAfterEnv: ['./jest.setup.ts'],
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
 }
-export default config
 
-// module.exports = {
-//     transform: {
-//         '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
-//     },
-//     collectCoverageFrom: [
-//         'siteconfig.js',
-//         'actions/**/*.{js,jsx,ts,tsx}',
-//         'libs/**/*.{js,jsx,ts,tsx}',
-//         '!src/**/*.d.ts',
-//     ],
-//     moduleNameMapper: {
-//         '^@config': '<rootDir>/config.js',
-//         '^@/actions/(.*)$': '<rootDir>/src/actions/$1',
-//         '^@/components/(.*)$': '<rootDir>/components/$1',
-//         '^@/reducers/(.*)$': '<rootDir>/src/reducers/$1',
-//         '^@/utils/(.*)$': '<rootDir>/utils/$1',
-//     },
-// }
+export default config
