09a1943535220eff051dd546b92bacad5257eef1
Garrett Bland <gmorganbland@gmail.com>
2022-06-12 22:12:23 -0500

    feat: updated types

--- types.ts
+++ types.ts
@@ -2,32 +2,35 @@
  * Common types used throughout entire project
  */
 
-export type Industries =
-    | 'financial'
-    | 'administrative'
-    | 'agriculture'
-    | 'construction'
-    | 'delivery'
-    | 'education'
-    | 'food_service'
-    | 'healthcare'
-    | 'hospitality'
-    | 'technology'
-    | 'custodial'
-    | 'manufacturing'
-    | 'professional'
-    | 'real_estate'
-    | 'retail'
-    | 'other'
+export enum Industries {
+    financial = 'financial',
+    administrative = 'administrative',
+    agriculture = 'agriculture',
+    construction = 'construction',
+    delivery = 'delivery',
+    education = 'education',
+    food_servie = 'food_service',
+    healthcare = 'healthcare',
+    hospitality = 'hospitality',
+    technology = 'technology',
+    custodial = 'custodial',
+    manufacturing = 'manufacturing',
+    professional = 'professional',
+    real_estate = 'real_estate',
+    retail = 'retail',
+    other = 'other',
+}
 
 export interface Job {
     id: string
+    employer_id: string
     title: string
     status: 'active' | 'inactive' | 'archived' | 'deleted'
     type: 'full_time' | 'part_time' | 'seasonal'
     industry: Industries
-    description: string
+    description?: string
     application_link?: string
+    promoted: boolean
     created_at: string
     updated_at: string
     expires_at: string
@@ -40,14 +43,16 @@ export interface Employer {
     status: 'active' | 'pending' | 'disabled' | 'deleted'
     contact: string
     email: string
-    phone: string
-    logo_url: string
-    website_url: string
-    facebook_url: string
-    twitter_url: string
+    phone?: string
+    users: {
+        user_id: string
+        role: 'admin' | 'member'
+        owner: boolean
+    }[]
+    logo_url?: string
+    website_url?: string
     created_at: string
     updated_at: string
-    // Need to add access and roles here?
 }
 
 export interface User {
@@ -60,4 +65,30 @@ export interface User {
     role: 'user' | 'employer' | 'moderator' | 'admin' // role might be saved in Employer? is_admin?
     created_at: string
     updated_at: string
+}
+
+export interface Subscriber {
+    id: string
+    email: string
+    status: 'active' | 'inactive'
+    created_at: string
+}
+
+export interface ContactSubmission {
+    id: string
+    first_name: string
+    last_name: string
+    email: string
+    phone: string
+    message: string
+    created_at: string
+    updated_at: string
+}
+
+export interface JobMetric {
+    id: string
+    job_id: string
+    device: 'mobile' | 'tablet' | 'desktop'
+    promoted_click: boolean
+    created_at: string
 }
